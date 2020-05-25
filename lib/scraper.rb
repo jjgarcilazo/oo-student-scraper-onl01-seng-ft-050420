0e4f83bcfb085856b76efad876564161448a3812
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-25 19:20:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,9 +7,9 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open('fixtures/student-site/index.html')
     doc = Nokogiri::HTML(html)
-    
+
     scraped_students = []
-    
+
     students = doc.css(".student-card")
     students.each do |student|
       name = student.css(".student-name").text
@@ -27,11 +27,11 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
-    
+
     student_profiles = {}
-   
+
     social_link = doc.css(".vitals-container .social-icon-container a")
-    
+
     social_link.each do |element|
       if element.attr("href").include?("twitter")
         student_profiles[:twitter] = element.attr('href')
@@ -43,10 +43,10 @@ class Scraper
       student_profiles[:blog] = element.attr("href")
       end
     end
-    
+
     student_profiles[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text
     student_profiles[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder p").text
-    
+
     student_profiles
   end
 end
